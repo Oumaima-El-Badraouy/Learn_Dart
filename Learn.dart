374bb8f40820b7a7f0eb95ff9ca951f9f86d9d17
oumaima <omaimaelbdraouy@gmail.com>
2026-01-01 17:01:57 +0100

    my first exercise in Dart (variables)

---
+++ Learn.dart
@@ -0,0 +1,11 @@
+
+void main(){
+  int age=20;
+  double height=1.8;
+  String name="omaima";
+  bool isStudent=true;
+  const pi=3.14;
+  final gravity=9.8;
+  var number=10;
+  print("you're age is $age and your height is $height");
+}
